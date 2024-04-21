7e2c096f68505f42d10786c6d446f0bb020784c2
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 02:06:30 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // write code here
+<script>
+        let username = prompt("Enter your username:");
+        let password = prompt("Enter your password:");
+
+        if (!username.trim()) {
+        alert("username is required");
+        } else if (!password.trim()) {
+        alert("password is required");
+        } else if ((username === "admin" && password === "1234") || (username === "john" && password === "qwerty")) {
+        alert("Hello " + username);
+        } else {
+        alert("invalid username or password");
+        }
+
+</script>
